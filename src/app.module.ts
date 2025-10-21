c5543b86df4f139c7bf6708cf617bb7e616fc1e9
Shambu T N <shambutn@gmail.com>
2025-10-21 20:01:32 +0530

    Deleted users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NinjaModule } from './ninja/ninja.module';
-import { UsersModule } from './users/users.module';
 @Module({
-  imports: [NinjaModule, UsersModule],
+  imports: [NinjaModule],
   controllers: [AppController],
   providers: [AppService],
 })
