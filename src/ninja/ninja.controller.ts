2382f95489cbd59099c1735158ce0a6ea65b8b73
Shambu T N <shambutn@gmail.com>
2025-10-22 13:06:58 +0530

    Controllers and dto

--- src/ninja/ninja.controller.ts
+++ src/ninja/ninja.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
 
 @Controller('ninja')
-export class NinjaController {}
+export class NinjaController {
+    @Get()
+    getNinjas() {
+        return 'List of ninjas';
+    }
+    @Get()
+    GetQuery(@Query('name') name: string) {
+        return `Ninja name is ${name}`;
+    }
+
+    @Get(':id')
+    getNinjaById(@Param('id') id: string) {
+        return `Details of a specific ninja ${id}`;
+    }
+    @Post()
+    postNinja(@Body() CreateNinjaDto: CreateNinjaDto) {
+        return {
+            name: CreateNinjaDto.name,
+        };
+    }
+    @Put(':id')
+    putNinja(@Param('id') id: string, @Body() CreateNinjaDto: CreateNinjaDto) {
+        return {
+            id,
+            name: CreateNinjaDto.name,
+        };
+    }
+}
